f45ce08083a12edbe2a2d019d0824779902a7f50
Thor1416 <ts4516@gmail.com>
2014-11-29 15:00:51 +0100

    Initial Upload of the project

---
+++ PingPong/src/com/thor/pingpong/Bold.java
@@ -0,0 +1,53 @@
+package com.thor.pingpong;
+
+import java.awt.*;
+
+public class Bold {
+	
+	/* Created: 15-11-2014
+	 * Created By: Thorbjørn Simonsen and Rasmus Ruff
+	 * Description:
+	 * 
+	 * Version  0.1a
+	 * Last Updated: 15-11-2014
+	 * */
+	
+	
+	// Variable Declaration
+	public int bstørrelse; //The Size of The ball
+	public double bflyveretning; //the direction of the ball 
+	public int bhastighed; // the speed of the ball
+	public double bvinkel; // the vinkel of the balls direction
+	public int posX; //"x" cordinatie of the position
+	public int posY; // "y" cordinate of the position
+	
+	public Bold (int størrelse, int positionX, int positionY){// constuctor
+		bstørrelse = størrelse; //Initialization of Variables
+		posX =  positionX;
+		bhastighed = 10; 
+		
+		bvinkel=Math.random()*360;
+		bhastighed=(int) (bvinkel - 180);
+		
+	}
+	public void paintComponent(Graphics g){
+		
+		int spillefladeHeight;
+		int spillefadeWidth;
+		
+		posX2=(int) (posX+bhastighed*Math.sin(bflyveretning));
+		posY2=(int) (posY+bhastighed*Math.sin(bflyveretning));
+		
+		if(posX2)
+		
+		g.drawOval(posX,posY,bstørrelse,bstørrelse);
+	}
+	public void skiftRetning(){
+		bhastighed = bhastighed-1;
+		double nyRetning;
+		
+		//nyRetning=180 gammel redning 
+	}
+	
+	
+}
---
+++ PingPong/src/com/thor/pingpong/Spilleflade.java
@@ -0,0 +1,14 @@
+package com.thor.pingpong;
+
+import javax.swing.*;
+
+public class Spilleflade extends JPanel {
+	
+	JFrame vindue = new Jframe("Ping Pong");
+
+	public void tegn(Graphics g)
+	{
+	 
+	}
+
+}
